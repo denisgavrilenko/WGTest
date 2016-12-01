6aca6c4a72ffdf82566d8f974d8bd818989c9a16
Denis Gavrilenko <Denis_Gavrilenko@cedoni.com>
2016-12-01 14:39:41 +0300

    [Proj] Add New Project

---
+++ WGTest/WGTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WGTest
+//
+//  Created by Denis Gavrilenko on 12/1/16.
+//  Copyright © 2016 DreamTeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
